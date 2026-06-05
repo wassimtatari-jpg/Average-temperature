ca4907c65db509f2ab248e3dac1ffb829d775a06
Mohamed Wassim Tatari <wassimtatari@gmail.com>
2026-06-05 13:08:38 +0100

    average temperaturs 2 in zip

---
+++ average temperaturs 2 in  zip .py
@@ -0,0 +1,18 @@
+print("welcome to average calculater temperatures")\
+
+print("to calculate the 7 days average temperatures Enter them ")
+
+days=("Saturday","Sunday","Monday","Tusday","Wednsday","Thursday","friday")
+
+temps=tuple(float(input(f"{day} Temperature : ")) for day in days)
+
+sorted_temp=sorted(temps)
+
+average_temp=sum(sorted_temp)/len(sorted_temp)
+
+print("Report of temperature of this week : ")
+
+for day,temp in zip(days,temps):
+    print(f"{day} : {temp}")
+
+print(f"The average of temperature of this week is  {average_temp:.2f}")
